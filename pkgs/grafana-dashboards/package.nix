820f15887d68bc6b98e6fa4af5099c9c65e8e994
tetra <me@tetra.cool>
2026-05-05 13:41:38 -0700

    feat(grafana-dashboards): make overridable

--- pkgs/grafana-dashboards/package.nix
+++ pkgs/grafana-dashboards/package.nix
@@ -2,6 +2,7 @@
   lib,
   fetchurl,
   jq,
+  gnused,
   runCommand,
 }: let
   dashboards = lib.importJSON ./dashboards.json;
@@ -10,33 +11,36 @@
     id,
     revision,
     hash,
-  }: let
-    raw = fetchurl {
-      name = "grafana-dashboard-${slug}.raw.json";
-      url = "https://grafana.com/api/dashboards/${toString id}/revisions/${toString revision}/download";
-      inherit hash;
-    };
-
-    # deterministic uids: upstream dashboard `uid`s may collide with other dashboards.
-    # to solve this, derive a stable, slug-keyed uid so grafana provisioning is
-    # idempotent across revision bumps and authors
-    uid = builtins.substring 0 14 (builtins.hashString "sha256" slug);
-  in
-    runCommand "grafana-dashboard-${slug}.json" {
-      nativeBuildInputs = [jq];
-
-      passthru = {
-        inherit id revision uid raw;
-        grafanaSlug = slug;
-        updateScript = ./update.sh;
+  }:
+    lib.makeOverridable ({datasource ? "prometheus"}: let
+      raw = fetchurl {
+        name = "grafana-dashboard-${slug}.raw.json";
+        url = "https://grafana.com/api/dashboards/${toString id}/revisions/${toString revision}/download";
+        inherit hash;
       };
 
-      meta = {
-        description = "Grafana community dashboard: ${slug}";
-        homepage = "https://grafana.com/grafana/dashboards/${toString id}/";
-      };
-    } ''
-      jq --arg uid '${uid}' '.uid = $uid' ${raw} > $out
-    '';
+      # deterministic uids: upstream dashboard `uid`s may collide with other dashboards.
+      # to solve this, derive a stable, slug-keyed uid so grafana provisioning is
+      # idempotent across revision bumps and authors
+      uid = builtins.substring 0 14 (builtins.hashString "sha256" slug);
+    in
+      runCommand "grafana-dashboard-${slug}.json" {
+        nativeBuildInputs = [jq gnused];
+
+        passthru = {
+          inherit id revision uid raw datasource;
+          grafanaSlug = slug;
+          updateScript = ./update.sh;
+        };
+
+        meta = {
+          description = "Grafana community dashboard: ${slug}";
+          homepage = "https://grafana.com/grafana/dashboards/${toString id}/";
+        };
+      } ''
+        jq --arg uid '${uid}' '.uid = $uid' ${raw} \
+          | sed 's|''${DS_PROMETHEUS}|${datasource}|g' \
+          > $out
+      '') {};
 in
   lib.recurseIntoAttrs (lib.mapAttrs mkDashboard dashboards)
